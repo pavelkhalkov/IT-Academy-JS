54405bc75fcd1673754805d183d45e5519a31a3b
pavelkhalkov <pavel.khalkov@mail.ru>
2022-09-26 15:11:18 +0400

    HomeWork4.Addition

--- test.js
+++ test.js
@@ -15,19 +15,19 @@ fs.ensureDirSync( dire, './testFolder2');
 //4. Переместить файл из первой во вторую папку
 fs.moveSync('./testFolder1/file.txt', './testFolder2/file.txt')
 
-// 5. Создать третью папку
+//5. Создать третью папку
 const direс = './testFolder3'
 fs.ensureDirSync( direс, './testFolder3');
 
-//6. Cкопировать файл из второй в третью папку
+//6.Cкопировать файл из второй в третью папку
 const direсt = './testFolder3'
 fs.copySync('./testFolder2/file.txt', './testFolder3/file.txt')
 
-// 7. Удалить файлы
+//7. Удалить файлы
 fs.removeSync('./testFolder2/file.txt')
 fs.removeSync('./testFolder3/file.txt')
 
-// // 8. Удалить все папки
+//8. Удалить все папки
 fs.removeSync('./testFolder1')
 fs.removeSync('./testFolder2')
 fs.removeSync('./testFolder3')
