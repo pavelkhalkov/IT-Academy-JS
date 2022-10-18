bb1f1dcb1b2c75e27da2702eaa7f6a909046aba7
pavelkhalkov <pavel.khalkov@mail.ru>
2022-10-18 16:50:23 +0300

    Update

--- lesson8/task1.js
+++ lesson8/task1.js
@@ -1,4 +1,11 @@
 //1. Поменять массив в обратном порядке - [1,2,3,4,5,6] [6,5,4,3,2,1]
 
-const arr = [1,2,3,4,5,6]; 
-console.log(arr.reverse());
+let arr = [1, 2, 3, 4, 5, 6],
+    inverse;
+
+inverse = arr.reduce(function (carrier, element) {
+    carrier.unshift(element);
+    return carrier;
+}, []);
+
+console.log(inverse);
