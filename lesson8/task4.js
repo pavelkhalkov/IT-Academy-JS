7b9653b5662a2798523391b2295e2601c45a2a6d
pavelkhalkov <pavel.khalkov@mail.ru>
2022-10-18 16:14:19 +0300

    UPT

--- lesson8/task4.js
+++ lesson8/task4.js
@@ -1,10 +1,12 @@
 //4. Даны 2 4-х значных числа с неповторяющимися цифрами, надо определить сколько цифр в этих числах совпадают по значению и позиции и сколько только по значению (3487 и 3794 ---> 1 и 2 )
 
-let number1 = 1142;
-let number2 = 5121;
+let number1 = 3487;
+let number2 = 3794;
 
-let arr1 = Array.from(number1.toString(), Number)
-let arr2 = Array.from(number2.toString(), Number)
+let arr1 = Array.from(number1.toString(), Number);
+console.log(arr1)
+let arr2 = Array.from(number2.toString(), Number);
+console.log(arr2)
 
 let a = 0;
 let b = 0;
@@ -12,9 +14,9 @@ let b = 0;
 function f1 (element, index, array)
 {
     arr2.forEach(function(item, i, arr) {
-       if (element==item){
+       if (element===item){
            a++
-           if(i==index){
+           if(index===i){
                b++
            }
        }
@@ -23,6 +25,3 @@ function f1 (element, index, array)
 
 arr1.forEach(f1);
 console.log("Количество повторов = "+a+"  Cовпадает позиций = "+b+" ");
-
-
-
