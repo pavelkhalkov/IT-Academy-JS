6fa54e661b7ace80a8fe7fa8af6d67339b2fbd86
pavelkhalkov <pavel.khalkov@mail.ru>
2022-11-05 23:56:21 +0300

    upd

--- lesson8/task1.js
+++
@@ -1,9 +0,0 @@
-//1. Поменять массив в обратном порядке - [1,2,3,4,5,6] [6,5,4,3,2,1]
-
-let arr = [1,2,3,4,5,6];
-
-function reverseArr(arr) {
-    return arr.reverse();
-  }
-
-console.log(reverseArr([1,2,3,4,5,6]));
